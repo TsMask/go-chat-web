38e6ca758c87acb7add73a54183d7c4864c8eae3
TsMask <340112800@qq.com>
2025-11-08 14:56:45 +0800

    style: 去除页面下脚多出的逗号

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Login from './chat/Login';
-import Panel from './chat/Panel';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './chat/redux/module/index';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Login from "./chat/Login";
+import Panel from "./chat/Panel";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./chat/redux/module/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/panel/:user" component={Panel} />
-        <Route path="/" component={Login} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/panel/:user" component={Panel} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
